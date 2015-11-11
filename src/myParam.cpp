febc94fb0e7e38057be39c48445d5a4f8a636bd7
alchemist231 <abhinav23194@gmail.com>
2015-11-11 15:55:23 +0530

    added myParam files : 11/11/2015

---
+++ src/myParam.cpp
@@ -0,0 +1,32 @@
+bool globalTrace = true;
+bool globalTraceBuffer = false ;
+bool globalTraceChannel = true;
+int globalTraceBufferCount = 0;
+bool globalTraceRoutingTable = true;
+bool globalTraceTrafficTable = true;
+bool globalTraceGlobalStats = true;
+bool globalTraceHub = true;
+int globalTraceHubCount = 0;
+bool globalTraceInitiator = false;
+bool globalTraceLocalRoutingTable = false;
+int globalTraceLocalRoutingTableCount= 0;
+bool globalTraceMm=true;
+bool globalTraceNoC=true;
+bool globalTracePower=false;
+bool globalTraceProcessingElement=false;
+
+/*
+buffer.cpp
+channel.h
+GlobalRoutingTable.cpp
+GlobalTrafficTable.cpp
+Hub.h
+Initiator.h
+LocalRoutingTable.cpp
+mm.cpp
+NoC.cpp
+power.cpp
+processingElement.cpp
+
+
+*/
---
+++ src/myParam.h
@@ -0,0 +1,16 @@
+extern bool globalTrace;
+extern bool globalTraceBuffer;
+extern int globalTraceBufferCount;
+extern bool globalTraceChannel;
+extern bool globalTraceRoutingTable;
+extern bool globalTraceTrafficTable;
+extern bool globalTraceGlobalStats;
+extern bool globalTraceHub; 
+extern int globalTraceHubCount;
+extern bool globalTraceInitiator;
+extern bool globalTraceLocalRoutingTable;
+extern int globalTraceLocalRoutingTableCount;
+extern bool globalTraceMm;
+extern bool globalTraceNoC;
+extern bool globalTracePower;
+extern bool globalTraceProcessingElement;
